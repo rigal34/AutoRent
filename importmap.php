96488560a994aaa83d621dddd7d0f7b910d11d09
rigal34 <SST.plomberie@laposte.net>
2025-08-10 17:49:30 +0100

    footer terminé

--- importmap.php
+++ importmap.php
@@ -35,4 +35,11 @@ return [
         'version' => '5.3.7',
         'type' => 'css',
     ],
+    '@fortawesome/fontawesome-free' => [
+        'version' => '7.0.0',
+    ],
+    '@fortawesome/fontawesome-free/css/fontawesome.min.css' => [
+        'version' => '7.0.0',
+        'type' => 'css',
+    ],
 ];
